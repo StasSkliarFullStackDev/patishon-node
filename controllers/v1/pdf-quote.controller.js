5f2d58f65523a92c0cce3ef6701da7cb3d896269
Stas <stasdev@empireads.dev>
2024-08-12 19:38:37 +0300

    change quote PDF

--- controllers/v1/pdf-quote.controller.js
+++ controllers/v1/pdf-quote.controller.js
@@ -54,6 +54,7 @@ const imageAndPdfGenerator = async (req, res) => {
             20 +
             glassCoveringPrice;
     }
+    totalPrice = (totalPrice + 300) * 20 / 100
 
     const data = {
         orderId,
